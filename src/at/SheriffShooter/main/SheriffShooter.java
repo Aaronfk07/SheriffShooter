7b0a1307a3c5ac7564198a0530b83369ac5245f7
Aaronfk07 <ghost.aaron@gmx.at>
2025-04-11 11:34:51 +0200

    Initialize SheriffShooter as a Slick2D game and implement main game structure.

--- src/at/SheriffShooter/main/SheriffShooter.java
+++ src/at/SheriffShooter/main/SheriffShooter.java
@@ -1,4 +1,35 @@
 package at.SheriffShooter.main;
 
-public class SheriffShooter {
+import org.newdawn.slick.*;
+
+public class SheriffShooter extends BasicGame {
+    public SheriffShooter(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new SheriffShooter("SheriffShooter"));
+            container.setDisplayMode(800, 600, false);
+            container.setShowFPS(false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
 }
